1cf88c66b0e98c59c18d9959e70780055393d9e2
PirisRu <70095816+PirisRu@users.noreply.github.com>
2023-07-03 13:08:49 +0300

    fix jwt pw

--- BackEnd/utils.js
+++ BackEnd/utils.js
@@ -16,12 +16,12 @@ export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
     const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
-    jwt.verify(token, process.env.SECRET_KEY, (err, decode) => {
+    jwt.verify(token, process.env.JWT_PW, (err, decode) => {
       if (err) {
         console.log('Invalid Token');
         res.status(401).send({ message: 'Invalid Token' });
       } else {
-        console.log("valid token")
+        console.log('valid token');
         req.user = decode;
         next();
       }
--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -10,7 +10,7 @@ import SearchPage from './pages/SearchPage/SearchPage'
 function App () {
   return (
     <div className='App'>
-      <BrowserRouter>
+      <BrowserRouter >
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route exact path='/login' element={<LoginPage />} />
